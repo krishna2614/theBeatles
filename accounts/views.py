49669cb2cd75c25df62a4347397409270ddedc8a
krishna2614 <148428995+krishna2614@users.noreply.github.com>
2023-10-19 14:33:32 -0500

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from . import forms
+
+
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        message = None
+        form = forms.SigninForm()
+        if request.method == 'POST':
+            form = forms.SigninForm(request.POST)
+            if form.is_valid():
+                user = authenticate(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password'],
+                )
+                if user is not None:
+                    login(request, user)
+                    message = f'Hello {user.username}! You have been logged in'
+                    return redirect('index')
+                else:
+                    message = 'Login failed!'
+        return render(request, 'signin.html', context={'form': form, 'message': message})
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = forms.SignupForm()
+        if request.method == 'POST':
+            form = forms.SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                # auto-login user
+                login(request, user)
+                return redirect('accounts_signin')
+        return render(request, 'register.html', context={'form': form})
+
+
+def _logout(request):
+    logout(request)
+    return redirect('accounts_signin')
